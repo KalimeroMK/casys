4d78970cbabd4586b58e11caa7498d04c3b88948
KalimeroMK <zbogoevski@gmail.com>
2024-05-18 19:31:45 +0200

    Some dependency update and add some tests

--- tests/Feature/CasysTest.php
+++ tests/Feature/CasysTest.php
@@ -2,15 +2,55 @@
 
 namespace Kalimero\Casys\Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Kalimero\Casys\Http\Controllers\CasysController;
 use Orchestra\Testbench\TestCase;
 
 class CasysTest extends TestCase
-
 {
-    public function testBasic()
+    use RefreshDatabase, WithFaker;
+
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+    }
+
+    public function test_it_loads_the_casys_loader_view_on_index()
+    {
+        $response = $this->get(route('casys.index'));
 
         $response->assertStatus(200);
+        $response->assertViewIs('casys::loader');
+    }
+
+    public function test_it_returns_casys_data_on_getCasys()
+    {
+        $client = $this->faker->word;
+        $amount = $this->faker->randomNumber(2);
+
+        $response = $this->get(route('casys.getCasys', ['client' => $client, 'amount' => $amount]));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('casys::index');
+        $response->assertViewHas('casys');
+    }
+
+    public function test_it_returns_success_view_on_success()
+    {
+        $response = $this->get(route('casys.success'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('casys::okurl');
+        $response->assertViewHas('success', 'You transaction was successful');
+    }
+
+    public function test_it_returns_fail_view_on_fail()
+    {
+        $response = $this->get(route('casys.fail'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('casys::failurl');
+        $response->assertViewHas('success', 'Your transaction failed');
     }
 }
